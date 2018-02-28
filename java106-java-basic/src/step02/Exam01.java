adaaab64d5060f86f87153fada50b7c4c2ff461e
eomjinyoung <jinyoung.eom@gmail.com>
2018-02-28 12:18:59 +0900

    '.'

--- java106-java-basic/src/step02/Exam01.java
+++ java106-java-basic/src/step02/Exam01.java
@@ -1,5 +1,9 @@
+// 패키지 
+
+package step02;
+ 
 public class Exam01 {
     public static void main(String[] args) {
-        System.out.println("src/step01/Exam02");
+        System.out.println("src/step02/Exam01");
     }
 }
