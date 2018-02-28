ec68c5290fa86e5ce3be13ceed2ddf4b6ac2c7e4
eomjinyoung <jinyoung.eom@gmail.com>
2018-02-28 17:14:01 +0900

    '.'

--- java106-java-basic/src/step01/Exam04.java
+++ java106-java-basic/src/step01/Exam04.java
@@ -3,11 +3,27 @@ package step01;
 
 public class Exam04 {
     public static void main(String[] args) {
-        // 숫자 
+        // 정수 
+        // => 4바이트 크기의 정수
         System.out.println(10);
         System.out.println(+10);
         System.out.println(-10);
+        
+        // => 8바이트 크기의 정수
+        //    숫자 뒤에 l, L을 붙인다.
+        System.out.println(10L);
+        System.out.println(+10l);
+        System.out.println(-10L);
+
+        // 부동소수점
+        // => 4바이트 크기
+        //    숫자 뒤에 F, f를 붙인다.
+        System.out.println(3.14F);
+        System.out.println(3.14f);
+        
+        // => 8바이트 크기
         System.out.println(3.14);
+        System.out.println(31.4e-1);
 
         // 문자열
         System.out.println("오호라..");
