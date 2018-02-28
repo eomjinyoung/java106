a03942e4fa5c158b1b900f4c1d944d9bb7ae7259
eomjinyoung <jinyoung.eom@gmail.com>
2018-02-28 12:56:25 +0900

    '.'

---
+++ java106-java-basic/src/step01/Exam04.java
@@ -0,0 +1,27 @@
+// 리터럴(literal)
+package step01;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        // 숫자 
+        System.out.println(10);
+        System.out.println(+10);
+        System.out.println(-10);
+        System.out.println(3.14);
+
+        // 문자열
+        System.out.println("오호라..");
+
+        // 한 개의 문자
+        System.out.println('가');
+
+        // 논리값 
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
+
+// 자바 코드로 값을 표현 것  
+// 
