7d0f31545be9153850d504c7b491587472e18953
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-02 17:33:29 +0900

    '.'

---
+++ java106-java-basic/src/step01/Exam07_2.java
@@ -0,0 +1,13 @@
+// 문자의 리터럴 - 크기
+package step01;
+
+public class Exam07_1 {
+    public static void main(String[] args) {
+        
+        // 문자(영어, 한글, 한자 상관없이)는 2바이트 메모리를 사용한다.
+        // 문자 코드는 음수가 없으므로,
+        // 0 ~ 65535까지의 크기를 갖는다.
+        System.out.println('A'); 
+
+    }
+}
---
+++ java106-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -0,0 +1,7 @@
+package bitcamp.java106.pms;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
