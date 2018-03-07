54a897226114bb1c7d9894df0f0b02689ff71b00
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-07 17:08:28 +0900

    .

---
+++ java106-java-basic/src/step04/Exam01_4.java
@@ -0,0 +1,11 @@
+// 산술 연산자 : 연산의 결과 타입  
+package step04;
+
+public class Exam01_4 {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 2;
+        float r = i / j;
+        System.out.println(r);
+    }
+}
