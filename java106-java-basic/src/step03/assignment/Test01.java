13a28c7e0157e315e2bf4eb6d0b03ed0b97157ca
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-07 13:03:56 +0900

    .

---
+++ java106-java-basic/src/step03/assignment/Test01.java
@@ -0,0 +1,10 @@
+package step03.assignment;
+
+public class Test01 {
+    public static void main(String[] args) {
+        java.util.Scanner keyScan = new java.util.Scanner(System.in);
+
+        System.out.print("입력: ");
+        String name = keyScan.next();
+    }
+}
