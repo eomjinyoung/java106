880b96e0ca03fc8ba4bf7f6b1ec0703d694fc7e0
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-09 11:46:06 +0900

    .

--- java106-java-basic/src/step04/Exam04_1.java
+++ java106-java-basic/src/step04/Exam04_1.java
@@ -1,29 +1,24 @@
 // 비트 이동 연산자 : >>, >>>, << 
 package step04;
 
-public class Exam03_1 {
+public class Exam04_1 {
     public static void main(String[] args) {
-        // AND 연산자 
-        // - 두 개의 논리 값이 모두 true일 때 결과가 true가 된다.
-        System.out.println(true && true);
-        System.out.println(true && false);
-        System.out.println(false && true);
-        System.out.println(false && false);
+        int i = 1;
+        System.out.println(i << 1);
+        System.out.println(i << 2);
+        System.out.println(i << 3);
+        System.out.println(i << 4);
+        // 왼쪽으로 이동 후, 
+        // 오른 쪽 빈자리 : 0으로 채운다.
+        // 왼쪽 경계를 넘어간 비트 : 짜른다.
 
-        System.out.println("-----------------------");
-        
-        // OR 연산자 
-        // - 두 개의 논리 값 중 한 개라도 true이면 결과는 true가 된다.
-        System.out.println(true || true);
-        System.out.println(true || false);
-        System.out.println(false || true);
-        System.out.println(false || false);
-
-        System.out.println("-----------------------");
-        
-        // NOT 연산자 
-        // - true는 false로 false는 true로 바꾼다.
-        System.out.println(!true);
-        System.out.println(!false);
+        i = 0b0000_1011; // 11
+        System.out.println(i << 1); // 22 => 00010110
+        System.out.println(i << 2); // 44 => 00101100
+        System.out.println(i << 3); // 88 => 01011000
     }
 }
+
+// 왼쪽 이동
+// - 1비트 이동은 곱하기 2 한 것과 같은 효과를 준다.
+// 
---
+++ java106-java-basic/src/step04/Exam04_4.java
@@ -0,0 +1,12 @@
+// 비트 이동 연산자 : >>, >>>, << 
+package step04;
+
+public class Exam04_4 {
+    public static void main(String[] args) {
+        int i = 0x27a130ff;
+        System.out.println(Integer.toHexString(i));
+
+        // i 변수에 들어있는 값을 1바이트씩 짤라서 16진수로 출력하라!
+        
+    }
+}
