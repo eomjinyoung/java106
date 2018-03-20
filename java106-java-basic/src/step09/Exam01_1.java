7e924d1a63433fd1cc92e6fb553d2371a9849a0e
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-20 12:59:03 +0900

    .

---
+++ java106-java-basic/src/step09/Exam01_1.java
@@ -0,0 +1,11 @@
+// 생성자 활용 예 - 자바에서 제공하는 클래스 사용을 통해 생성자 활용을 익혀보자!
+package step09;
+
+public class Exam01_1 {
+
+    public static void main(String[] args) {
+        String s1 = new String("Hello");
+
+    }
+
+}
