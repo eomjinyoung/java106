0e58bce9d90a2f7695ce9adfb18f3892859bf708
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-23 12:34:34 +0900

    .

---
+++ java106-java-project/src15/main/java/bitcamp/java106/pms/controller/BoardController.java
@@ -0,0 +1,126 @@
+package bitcamp.java106.pms.controller;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+import bitcamp.java106.pms.dao.BoardDao;
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.util.Console;
+
+public class BoardController {
+    Scanner keyScan;
+
+    BoardDao boardDao = new BoardDao();
+    
+    public BoardController(Scanner scanner) {
+        this.keyScan = scanner;
+    }
+    
+    public void service(String menu, String option) {
+        if (menu.equals("board/add")) {
+            this.onBoardAdd();
+        } else if (menu.equals("board/list")) {
+            this.onBoardList();
+        } else if (menu.equals("board/view")) {
+            this.onBoardView(option);
+        } else if (menu.equals("board/update")) {
+            this.onBoardUpdate(option);
+        } else if (menu.equals("board/delete")) {
+            this.onBoardDelete(option);
+        } else {
+            System.out.println("명령어가 올바르지 않습니다.");
+        }
+    }
+
+    void onBoardAdd() {
+        System.out.println("[게시물 입력]");
+        Board board = new Board();
+
+        System.out.print("제목? ");
+        board.title = this.keyScan.nextLine();
+
+        System.out.print("내용? ");
+        board.content = this.keyScan.nextLine();
+
+        System.out.print("등록일? ");
+        board.createdDate = Date.valueOf(this.keyScan.nextLine());
+
+        boardDao.insert(board);
+    }
+
+    void onBoardList() {
+        System.out.println("[게시물 목록]");
+        Board[] list = boardDao.list();
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == null) continue;
+            System.out.printf("%d, %s, %s\n",
+                i, list[i].title, list[i].createdDate);
+        }
+    }
+
+    void onBoardView(String option) {
+        System.out.println("[게시물 조회]");
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Board board = boardDao.get(Integer.parseInt(option));
+        
+        if (board == null) {
+            System.out.println("유효하지 않은 게시물 번호입니다.");
+        } else {
+            System.out.printf("팀명: %s\n", board.title);
+            System.out.printf("설명: %s\n", board.content);
+            System.out.printf("등록일: %s\n", board.createdDate);
+        }
+    }
+
+    void onBoardUpdate(String option) {
+        System.out.println("[게시물 변경]");
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Board board = boardDao.get(Integer.parseInt(option));
+        
+        if (board == null) {
+            System.out.println("유효하지 않은 게시물 번호입니다.");
+        } else {
+            Board updateBoard = new Board();
+            System.out.printf("제목(%s)? ", board.title);
+            updateBoard.title = this.keyScan.nextLine();
+            System.out.printf("설명(%s)? ", board.content);
+            updateBoard.content = this.keyScan.nextLine();
+            updateBoard.createdDate = board.createdDate;
+            updateBoard.no = board.no;
+            boardDao.update(updateBoard);
+            System.out.println("변경하였습니다.");
+        }
+    }
+
+    void onBoardDelete(String option) {
+        System.out.println("[게시물 삭제]");
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return; 
+        }
+        
+        int i = Integer.parseInt(option);
+        Board board = boardDao.get(i);
+        
+        if (board == null) {
+            System.out.println("유효하지 않은 게시물 번호입니다.");
+        } else {
+            if (Console.confirm("정말 삭제하시겠습니까?")) {
+                boardDao.delete(i);
+                System.out.println("삭제하였습니다.");
+            }
+        }
+    }
+    
+}
+
+// ver 14 - BoardDao를 사용하여 게시물 데이터를 관리한다.
+// ver 13 - 게시물 등록할 때 등록일의 문자열을 Date 객체로 만들어 저장.
---
+++ java106-java-project/src15/main/java/bitcamp/java106/pms/dao/MemberDao.java
@@ -0,0 +1,60 @@
+package bitcamp.java106.pms.dao;
+
+import bitcamp.java106.pms.domain.Member;
+
+public class MemberDao {
+    Member[] members = new Member[1000];
+    int memberIndex = 0;
+    
+    public void insert(Member member) {
+        // 회원 정보가 담겨있는 객체의 주소를 배열에 보관한다.
+        this.members[this.memberIndex++] = member;
+    }
+    
+    public Member[] list() {
+        Member[] arr = new Member[this.memberIndex];
+        for (int i = 0; i < this.memberIndex; i++)
+            arr[i] = this.members[i];
+        return arr;
+    }
+    
+    public Member get(String id) {
+        int i = this.getMemberIndex(id);
+        if (i == -1)
+            return null;
+        return this.members[i];
+    }
+    
+    public void update(Member member) {
+        int i = this.getMemberIndex(member.id);
+        if (i != -1)
+            this.members[i] = member;
+    }
+    
+    public void delete(String id) {
+        int i = this.getMemberIndex(id);
+        if (i != -1)
+            this.members[i] = null;
+    }
+    
+    // 다음 메서드는 내부에서만 사용할 것이기 때문에 공개하지 않는다.
+    private int getMemberIndex(String id) {
+        for (int i = 0; i < this.memberIndex; i++) {
+            if (this.members[i] == null) continue;
+            if (id.equals(this.members[i].id.toLowerCase())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    
+}
+
+//ver 14 - MemberController로부터 데이터 관리 기능을 분리하여 MemberDao 생성.
+
+
+
+
+
+
