ab9c5181668f0b5becec51b1d0808ef53870aa64
eomjinyoung <jinyoung.eom@gmail.com>
2018-03-30 15:38:27 +0900

    .

--- java106-java-basic/src/step12/ex6/Exam03_2.java
+++ java106-java-basic/src/step12/ex6/Exam03_2.java
@@ -1,10 +1,10 @@
-// java.util.HashMap - value 목록만 꺼내기  
+// HashMap과 Iterator
 package step12.ex6;
 
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
-public class Exam02_3 {
+public class Exam03_2 {
     public static void main(String[] args) {
         Member v1 = new Member("홍길동", 20);
         Member v2 = new Member("임꺽정", 30);
@@ -19,11 +19,18 @@ public class Exam02_3 {
         map.put("s04", v4);
         map.put("s05", v5);
         
-        Collection values = map.values();
-        for (Object value : values) {
-            System.out.println(value);
-        }
+        Set keys = map.keySet();
         
+        map.remove("s01");
+        map.remove("s02");
+        map.remove("s03");
+        
+        // Set 객체를 통해 key 를 꺼낼 때,
+        // 그 순간의 HashSet에 있는 key를 꺼낸다.
+        // 즉 keySet()을 호출할 때 모든 key를 미리 목록을 만들어 리턴하지 않는다.
+        for (Object key : keys) {
+            System.out.println(key);
+        }
     }
 
 }
