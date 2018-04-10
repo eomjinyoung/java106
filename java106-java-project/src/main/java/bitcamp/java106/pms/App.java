ea63fc3b9e795e3e5f1e2b641bf3223d07525751
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-10 12:45:32 +0900

    .

--- java106-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -28,16 +28,26 @@ public class App {
         TaskDao taskDao = (TaskDao) iocContainer.getBean(TaskDao.class);
         TeamDao teamDao = (TeamDao) iocContainer.getBean(TeamDao.class);
         TeamMemberDao teamMemberDao = (TeamMemberDao) iocContainer.getBean(TeamMemberDao.class);
-        try {
-            boardDao.save();
-            classroomDao.save();
-            memberDao.save();
-            taskDao.save();
-            teamDao.save();
-            teamMemberDao.save();
-        } catch (Exception e) {
-            System.out.println("게시물 데이터 저장 중 오류 발생!");
-        }
+        
+        // 각각의 데이터 저장에 대해 예외 처리를 분리해야 한다.
+        // 그래야만 예외가 발생하더라도 다른 데이터 저장은 정상적으로 수행할 것이다.
+        try {boardDao.save();} 
+        catch (Exception e) { System.out.println("게시물 데이터 저장 중 오류 발생!");}
+        
+        try {classroomDao.save();} 
+        catch (Exception e) { System.out.println("수업 데이터 저장 중 오류 발생!");}
+        
+        try {memberDao.save();} 
+        catch (Exception e) { System.out.println("회원 데이터 저장 중 오류 발생!");}
+        
+        try {taskDao.save();} 
+        catch (Exception e) { System.out.println("작업 데이터 저장 중 오류 발생!");}
+            
+        try {teamDao.save();} 
+        catch (Exception e) { System.out.println("팀 데이터 저장 중 오류 발생!");}    
+        
+        try {teamMemberDao.save();} 
+        catch (Exception e) { System.out.println("팀멤버 데이터 저장 중 오류 발생!");}
     }
 
     static void onHelp() {
@@ -79,16 +89,25 @@ public class App {
             } else if (menu.equals("help")) {
                 onHelp();
             } else {
-                int slashIndex = menu.lastIndexOf("/");
-                String controllerKey = (slashIndex < 0) ? 
-                        menu : menu.substring(0, slashIndex);
-                
-                Controller controller = (Controller) iocContainer.getBean(controllerKey);
-                
-                if (controller != null) {
-                    controller.service(menu, option);
-                } else {
-                    System.out.println("명령어가 올바르지 않습니다.");
+                try {
+                    int slashIndex = menu.lastIndexOf("/");
+                    String controllerKey = (slashIndex < 0) ? 
+                            menu : menu.substring(0, slashIndex);
+                    
+                    Controller controller = (Controller) iocContainer.getBean(controllerKey);
+                    
+                    if (controller != null) {
+                        controller.service(menu, option);
+                    } else {
+                        System.out.println("명령어가 올바르지 않습니다.");
+                    }
+                } catch (Exception e) {
+                    if (keyScan.hasNextLine()) { 
+                        // 키보드 입력으로 남은 잔여 데이터가 있다면 읽어서 버린다.
+                        keyScan.nextLine(); 
+                    }
+                    System.out.println("작업 실행 중에 오류가 발생하였습니다.");
+                    System.out.println("명령을 다시 실행해주세요!");
                 }
             }
 
@@ -97,6 +116,7 @@ public class App {
     }
 }
 
+//ver 25 - 예외 처리 코드 추가.
 //ver 24 - 파일 저장 기능 호출. 멤버 및 팀 데이터를 준비하는 메서드 제거.
 //ver 17 - Task 관리 기능 추가
 // ver 15 - TeamDao와 MemberDao 객체 생성. 
