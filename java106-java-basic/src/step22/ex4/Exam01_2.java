5846b444717c69c15e34358ad71a5ec6c188307d
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-11 16:00:47 +0900

    .

--- java106-java-basic/src/step22/ex4/Exam01_2.java
+++ java106-java-basic/src/step22/ex4/Exam01_2.java
@@ -13,8 +13,22 @@ public class Exam01_2 {
         member = new Member();
         
         // 1) 이름 읽기
-        // 2) 나이 읽기
+        byte[] buf = new byte[100];
+        int size = in.read(); // 이름이 저장된 바이트 배열의 수
+        in.read(buf, 0, size); // 이름 배열 개수 만큼 바이트를 읽어 배열에 저장한다.
+        member.name = new String(buf, 0, size, "UTF-8");
+        
+        // 2) 나이(int) 읽기
+        member.age = in.read() << 24;
+        member.age += in.read() << 16;
+        member.age += in.read() << 8;
+        member.age += in.read();
+        
         // 3) 성별 읽기
+        if (in.read() == 1) 
+            member.gender = true;
+        else 
+            member.gender = false;
         
         in.close();
         
