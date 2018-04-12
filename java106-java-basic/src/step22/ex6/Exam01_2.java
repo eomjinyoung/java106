7490febb1e54fb065e393a175dbf8db854d4d1f3
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-12 12:57:33 +0900

    .

--- java106-java-basic/src/step22/ex6/Exam01_2.java
+++ java106-java-basic/src/step22/ex6/Exam01_2.java
@@ -16,6 +16,13 @@ public class Exam01_2 {
         
         Member member = new Member();
         
+        // 바이트 배열의 값을 읽어서 String이나 int, boolean 값으로 만들려면 
+        // 다음과 같이 비트이동 연산 등을 수행해야 한다.
+        // 매번 이런 식으로 코딩을 해야 한다면 
+        // 읽어들일 항목이 많을 경우 코드가 매우 복잡할 것이다.
+        // 해결책? ByteArrayInputStream을 상속 받아 이런 기능을 수행하는 
+        // 메서드를 추가하라! DataInputStream 클래스처럼 만들라!
+        
         // 문자열 바이트의 크기 
         int size = in.read();
         
