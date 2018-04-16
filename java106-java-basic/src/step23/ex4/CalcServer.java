cf91cb3e82af73eda13bfa3f1242094362d98962
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-16 15:37:33 +0900

    .

--- java106-java-basic/src/step23/ex4/CalcServer.java
+++ java106-java-basic/src/step23/ex4/CalcServer.java
@@ -1,4 +1,4 @@
-// 응용 - 계산기 서버 만들기
+// connectionless(=stateless) 방식 - 계산기 서버 만들기
 package step23.ex4;
 
 import java.io.DataInputStream;
@@ -6,6 +6,23 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+// connectionless
+// => 한 번 요청에 한 번 응답을 하는 방식
+//=> 단점: 
+//   매번 요청할 때마다 서버에 연결해야 하기 때문에 실행 시간이 많이 걸린다. 
+//   실행시간 = 연결하는데 걸린 시간 + 데이터 전송 시간 + 서버 처리 시간 + 데이터 수신 시간 
+//   특히 서버에 연결하는데 시간이 많이 걸리기 때문에 실행 시간이 많이 소요된다.
+//=> 장점:
+//   서버에 계속 연결된 상태가 아니기 때문에 서버 입장에서는 메모리를 많이 사용하지 않는다.
+//   왜? 
+//   클라이언트와 연결을 계속 유지하지 않기 때문에 작업을 처리하는 동안만 연결정보를 유지한다.
+//   그래서 같은 메모리라도 connectionful 방식 보다는 
+//   더 많은 클라이언트의 요청을 처리할 수 있다.
+//=> 대표적인 예:
+//   HTTP 통신 - 웹브라우저가 서버에 연결한 후 요청을 하고 서버가 응답한 후 연결을 끊는다.
+//   메신저 - 메신저 서버에 연결하고 메시지 전송 후 연결 끊는다.
+//   메일 전송 - 메일서버에 데이터 전송 후 연결 끊는다.
+//
 public class CalcServer {
     public static void main(String[] args) throws Exception {
         System.out.println("서버 실행 중...");
