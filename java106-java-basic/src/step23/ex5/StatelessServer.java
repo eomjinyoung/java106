683247c17d8e99f96d44ee817c1971d94cddeed3
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-17 10:33:49 +0900

    .

--- java106-java-basic/src/step23/ex5/StatelessServer.java
+++ java106-java-basic/src/step23/ex5/StatelessServer.java
@@ -8,8 +8,10 @@ import java.util.Scanner;
 
 //stateless 방식의 특징
 //=> 요청 할 때마다 서버와 연결한다. 응답 받은 후 서버와의 연결을 끊는다. 
-//
+//=> 요청자를 구분할 수 없어, 클라이언트의 작업 결과를 보관할 수 없다.
 public class StatelessServer {
+    static int sum = 0;
+    
     public static void main(String[] args) throws Exception {
         System.out.println("서버 실행 중...");
         
@@ -30,16 +32,25 @@ public class StatelessServer {
             Scanner in = new Scanner(socket.getInputStream());
             ) {
             
+            String str = in.nextLine();
+            if (str.equals("")) {
+                out.println("결과 = " + sum);
+                return;
+            }
+
             // 클라이언트와 연결되면 클라이언트는 값을 한 개 보낸다. 
             // 서버는 그 클라이언트의 값을 기존 합계에 추가해야 한다.
-            int sum = 0;
-            
-            String str = in.nextLine();
-            if (str.equals(""))
-                out.println("결과 = " + sum);
-
             sum += Integer.parseInt(str);
             
+            // 클라이언트가 데이터를 보낼 때마다 새로 연결을 수행한다.
+            // => 데이터를 보낸 클라이언트가 누군지 서버에서 구분할 수 없다.
+            // => 이런 방식의 통신이 "stateless" 이다.
+            // => 클라이언트를 구분할 수 없기 때문에 각각의 클라이언트의 상태를 
+            //    따로 관리할 수 없다.
+            // 해결책?
+            // => 클라이언트가 접속할 때 식별번호를 발급하여 클라이언트를 구분한다.
+            // => 즉 서버로부터 식별번호를 부여받은 클라이언트는 
+            //    이후에 데이터를 보낼때 식별번호를 함께 보낸다.
         }
     }
 }
