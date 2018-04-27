0e1de65ec619bbae83c96845456634bc22b768c0
eomjinyoung <jinyoung.eom@gmail.com>
2018-04-27 12:22:25 +0900

    .

--- java106-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
@@ -1,8 +1,5 @@
 package bitcamp.java106.pms.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -10,28 +7,23 @@ import org.apache.ibatis.session.SqlSessionFactory;
 
 import bitcamp.java106.pms.annotation.Component;
 import bitcamp.java106.pms.domain.Board;
-import bitcamp.java106.pms.jdbc.DataSource;
 
 @Component
 public class BoardDao {
     
     SqlSessionFactory sqlSessionFactory;
-    DataSource dataSource;
     
-    public BoardDao(DataSource dataSource, SqlSessionFactory sqlSessionFactory) {
-        this.dataSource = dataSource;
+    public BoardDao(SqlSessionFactory sqlSessionFactory) {
         this.sqlSessionFactory = sqlSessionFactory;
     }
     
     public int delete(int no) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "delete from pms_board where bno=?");) {
-            
-            stmt.setInt(1, no);
-            return stmt.executeUpdate();
-        } 
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            int count = sqlSession.delete(
+                    "bitcamp.java106.pms.dao.BoardDao.delete", no);
+            sqlSession.commit();
+            return count;
+        }
     }
     
     public List<Board> selectList() throws Exception {
@@ -51,37 +43,18 @@ public class BoardDao {
     }
 
     public int update(Board board) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "update pms_board set titl=?, cont=?, cdt=now() where bno=?");) {
-            
-            stmt.setString(1, board.getTitle());
-            stmt.setString(2, board.getContent());
-            stmt.setInt(3, board.getNo());
-            return stmt.executeUpdate();
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            int count = sqlSession.update(
+                    "bitcamp.java106.pms.dao.BoardDao.update", board);
+            sqlSession.commit();
+            return count;
         }
     }
 
     public Board selectOne(int no) throws Exception {
-        try (
-            Connection con = dataSource.getConnection();
-            PreparedStatement stmt = con.prepareStatement(
-                "select bno,titl,cont,cdt from pms_board where bno=?");) {
-            
-            stmt.setInt(1, no);
-            
-            try (ResultSet rs = stmt.executeQuery();) {
-                if (!rs.next()) 
-                    return null;
-                
-                Board board = new Board();
-                board.setNo(rs.getInt("bno"));
-                board.setTitle(rs.getString("titl"));
-                board.setContent(rs.getString("cont"));
-                board.setCreatedDate(rs.getDate("cdt"));
-                return board;
-            }
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            return sqlSession.selectOne(
+                    "bitcamp.java106.pms.dao.BoardDao.selectOne", no);
         }  
     }
 }
