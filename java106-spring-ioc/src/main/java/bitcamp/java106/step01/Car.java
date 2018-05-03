b206ab5ccbd8fe69aa041642be1fe01586ba3088
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-03 12:20:50 +0900

    .

---
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step01/Car.java
@@ -0,0 +1,33 @@
+package bitcamp.java106.step01;
+
+public class Car {
+    String model;
+    String maker;
+    int cc;
+    
+    @Override
+    public String toString() {
+        return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + "]";
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getMaker() {
+        return maker;
+    }
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+    public int getCc() {
+        return cc;
+    }
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+    
+    
+}
--- java106-spring-ioc/src/main/java/bitcamp/java106/step01/Exam01.java
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step01/Exam01.java
@@ -1,9 +1,31 @@
+// 스프링 IoC 컨테이너 사용
 package bitcamp.java106.step01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Exam01 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // 스프링 IoC 컨테이너는 ApplicationContext를 구현한다.
+        ApplicationContext iocContainer = null;
+        
+        // ApplicationContext 구현체 종류
+        // 1) XML 파일에 작성된 설정 정보를 읽어서 처리하는 IoC 컨테이너
+        //    => ClassPathXmlApplicationContext (자바 classpath에서 xml 파일을 찾는다)
+        //    => FileSystemXmlApplicationContext (OS 파일시스템에서 xml 파일을 찾는다)
+        // 2) 클래스 안에 작성된 애노테이션을 읽어서 처리하는 IoC 컨테이너
+        //    => AnnotationConfigApplicationContext
+        // 
+        
+        
+        // ClassPathXmlApplicationContext 사용
+        iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step01/application-context.xml");
+        
+        Car car = iocContainer.getBean("c1");
+        
+        System.out.println(car);
 
     }
 
