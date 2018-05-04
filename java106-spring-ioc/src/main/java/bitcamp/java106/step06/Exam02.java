f1ffcf0b0fefaf7d9972bda5d6fdfa2d33bcb2ad
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-04 17:12:29 +0900

    .

--- java106-spring-ioc/src/main/java/bitcamp/java106/step06/Exam02.java
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step06/Exam02.java
@@ -1,20 +1,21 @@
-// 팩토리 메서드 호출 - static 메서드 호출
+// 팩토리 메서드 호출 - static 메서드 호출 응용 
 package bitcamp.java106.step06;
+
+import java.sql.Date;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import bitcamp.java106.BeanUtils;
 
-public class Exam01 {
+public class Exam02 {
 
     public static void main(String[] args) {
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-                "bitcamp/java106/step06/application-context-01.xml");
+                "bitcamp/java106/step06/application-context-02.xml");
         
-        System.out.println(iocContainer.getBean("c1"));
-        System.out.println(iocContainer.getBean("c2"));
-        System.out.println(iocContainer.getBean("c3"));
+        Date date = (Date) iocContainer.getBean("d1");
+        System.out.println(date);
     }
 
 }
--- java106-spring-ioc/src/main/java/bitcamp/java106/step06/Exam03.java
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step06/Exam03.java
@@ -1,20 +1,16 @@
-// 팩토리 메서드 호출 - static 메서드 호출
+// 팩토리 메서드 호출 - 인스턴스 메서드 호출
 package bitcamp.java106.step06;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import bitcamp.java106.BeanUtils;
-
-public class Exam01 {
+public class Exam03 {
 
     public static void main(String[] args) {
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-                "bitcamp/java106/step06/application-context-01.xml");
+                "bitcamp/java106/step06/application-context-03.xml");
         
         System.out.println(iocContainer.getBean("c1"));
-        System.out.println(iocContainer.getBean("c2"));
-        System.out.println(iocContainer.getBean("c3"));
     }
 
 }
