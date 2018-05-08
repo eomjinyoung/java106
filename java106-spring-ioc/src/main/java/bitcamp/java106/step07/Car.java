5de43c0b3df67fb3053d76b7567d0da1b6da94b6
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-08 11:09:25 +0900

    .

--- java106-spring-ioc/src/main/java/bitcamp/java106/step07/Car.java
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step07/Car.java
@@ -9,6 +9,9 @@ public class Car {
     boolean auto;
     Date createdDate;
     
+    public Car() {
+        System.out.println("Car()");
+    }
     
     @Override
     public String toString() {
@@ -19,30 +22,35 @@ public class Car {
         return auto;
     }
     public void setAuto(boolean auto) {
+        System.out.println("Car.setAuto()");
         this.auto = auto;
     }
     public Date getCreatedDate() {
         return createdDate;
     }
     public void setCreatedDate(Date createdDate) {
+        System.out.println("Car.setCreatedDate()");
         this.createdDate = createdDate;
     }
     public String getModel() {
         return model;
     }
     public void setModel(String model) {
+        System.out.println("Car.setModel()");
         this.model = model;
     }
     public String getMaker() {
         return maker;
     }
     public void setMaker(String maker) {
+        System.out.println("Car.setMaker()");
         this.maker = maker;
     }
     public int getCc() {
         return cc;
     }
     public void setCc(int cc) {
+        System.out.println("Car.setCc()");
         this.cc = cc;
     }
     
---
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step07/Exam03.java
@@ -0,0 +1,23 @@
+// 프로퍼티 에디터 - String ==> java.sql.Date ; 변환기 사용하여 처리하기
+package bitcamp.java106.step07;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java106.BeanUtils;
+
+public class Exam03 {
+
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step07/application-context-03.xml");
+        
+        System.out.println(iocContainer.getBean("c1"));
+    }
+
+}
+
+
+
+
+
