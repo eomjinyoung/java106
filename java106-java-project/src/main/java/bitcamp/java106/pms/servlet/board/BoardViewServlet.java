84217888ff6f9c659b95df6b5a5517af912f3f35
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-16 10:25:39 +0900

    .

--- java106-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardViewServlet.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardViewServlet.java
@@ -2,7 +2,6 @@ package bitcamp.java106.pms.servlet.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,8 +14,8 @@ import bitcamp.java106.pms.domain.Board;
 import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
-@WebServlet("/board/list")
-public class BoardListServlet extends HttpServlet {
+@WebServlet("/board/view")
+public class BoardViewServlet extends HttpServlet {
     
     BoardDao boardDao;
     
@@ -30,8 +29,9 @@ public class BoardListServlet extends HttpServlet {
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
-        // 출력할 때 String 객체의 값(UTF-16)을 어떤 문자표를 사용하여 인코딩해서 보낼 것인지 설정한다.
-        // => 반드시 출력 스트림을 얻기 전에 설정해야 한다.
+        //request.setCharacterEncoding("UTF-8");
+        int no = Integer.parseInt(request.getParameter("no"));
+        
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         
@@ -39,38 +39,41 @@ public class BoardListServlet extends HttpServlet {
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset='UTF-8'>");
-        out.println("<title>게시물 목록</title>");
+        out.println("<title>게시물 보기</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>게시물 목록</h1>");
+        out.println("<h1>게시물 보기</h1>");
+        
         try {
-            List<Board> list = boardDao.selectList();
-            
-            out.println("<p><a href='form.html'>새 글</a></p>");
-            out.println("<table border='1'>");
-            out.println("<tr>");
-            out.println("    <th>번호</th><th>제목</th><th>등록</th>");
-            out.println("</tr>");
-            for (Board board : list) {
-                out.println("<tr>");
-                out.printf("    <td>%d</td><td><a href='view?no=%d'>%s</a></td><td>%s</td>\n",
-                    board.getNo(), 
-                    board.getNo(),
-                    board.getTitle(), 
-                    board.getCreatedDate());
-                out.println("</tr>");
+            Board board = boardDao.selectOne(no);
+            if (board == null) {
+                throw new Exception("유효하지 않은 게시물 번호입니다.");
             }
+            out.println("<table border='1'>");
+            out.println("<tr><th>번호</th><td>");
+            out.printf("    <input type='text' value='%d'></td></tr>\n", 
+                    board.getNo());
+            out.println("<tr><th>제목</th>");
+            out.printf("    <td><input type='text' value='%s'></td></tr>\n",
+                    board.getTitle());
+            out.println("<tr><th>내용</th>");
+            out.printf("    <td><textarea rows='10' cols='60'>%s</textarea></td></tr>\n",
+                    board.getContent());
+            out.printf("<tr><th>등록일</th><td>%s</td></tr>\n", 
+                    board.getCreatedDate());
             out.println("</table>");
+            
         } catch (Exception e) {
-            out.println("<p>목록 가져오기 실패!</p>");
+            out.printf("<p>%s</p>\n", e.getMessage());
             e.printStackTrace(out);
         }
+        out.println("<p><a href='list'>목록</a></p>");
         out.println("</body>");
         out.println("</html>");
     }
 }
-
-//ver 37 - BoardListController를 서블릿으로 변경
+//ver 37 - BoardViewController를 서블릿으로 변경
+//         HTML로 출력 
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
-//ver 26 - BoardController에서 list() 메서드를 추출하여 클래스로 정의. 
+//ver 26 - BoardController에서 view() 메서드를 추출하여 클래스로 정의.
