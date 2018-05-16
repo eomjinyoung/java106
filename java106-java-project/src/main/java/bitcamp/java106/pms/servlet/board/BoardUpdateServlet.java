1c4c1a3b1302ade72a1df8b51069f8ce4a65a3e7
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-16 11:19:20 +0900

    .

--- java106-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardUpdateServlet.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/servlet/board/BoardUpdateServlet.java
@@ -2,7 +2,6 @@ package bitcamp.java106.pms.servlet.board;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import bitcamp.java106.pms.dao.BoardDao;
 import bitcamp.java106.pms.domain.Board;
-import bitcamp.java106.pms.server.ServerRequest;
-import bitcamp.java106.pms.server.ServerResponse;
 import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
@@ -32,28 +29,44 @@ public class BoardUpdateServlet extends HttpServlet {
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
-        PrintWriter out = response.getWriter();
+        request.setCharacterEncoding("UTF-8");
         
         Board board = new Board();
         board.setNo(Integer.parseInt(request.getParameter("no")));
         board.setTitle(request.getParameter("title"));
         board.setContent(request.getParameter("content"));
-        board.setCreatedDate(new Date(System.currentTimeMillis()));
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<title>게시물 변경</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>게시물 변경 결과</h1>");
         
         try {
             int count = boardDao.update(board);
             if (count == 0) {
-                out.println("해당 게시물이 존재하지 않습니다.");
+                out.println("<p>해당 게시물이 존재하지 않습니다.</p>");
             } else {
-                out.println("변경하였습니다.");
+                out.println("<p>변경하였습니다.</p>");
             }
         } catch (Exception e) {
-            out.println("변경 실패!");
+            out.println("<p>변경 실패!</p>");
             e.printStackTrace(out);
         }
+        out.println("</body>");
+        out.println("</html>");
     }
 }
 
+//ver 37 - BoardUpdateController를 서블릿으로 변경
+//         결과를 HTML로 출력
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
 //ver 26 - BoardController에서 update() 메서드를 추출하여 클래스로 정의.
