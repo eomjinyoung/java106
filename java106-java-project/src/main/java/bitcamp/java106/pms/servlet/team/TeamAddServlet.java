9166c9f1cca32f5fa0a888f6fc89279e8d63c7d0
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-16 16:56:02 +0900

    .

--- java106-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamAddServlet.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamAddServlet.java
@@ -1,8 +1,8 @@
-// Controller 규칙에 따라 메서드 작성
-package bitcamp.java106.pms.servlet.member;
+package bitcamp.java106.pms.servlet.team;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,32 +10,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bitcamp.java106.pms.dao.MemberDao;
-import bitcamp.java106.pms.domain.Member;
+import bitcamp.java106.pms.dao.TeamDao;
+import bitcamp.java106.pms.domain.Team;
 import bitcamp.java106.pms.servlet.InitServlet;
 
 @SuppressWarnings("serial")
-@WebServlet("/member/add")
-public class MemberAddServlet extends HttpServlet {
+@WebServlet("/team/add")
+public class TeamAddServlet extends HttpServlet {
 
-    MemberDao memberDao;
+    TeamDao teamDao;
     
     @Override
     public void init() throws ServletException {
-        memberDao = InitServlet.getApplicationContext().getBean(MemberDao.class);
+        teamDao = InitServlet.getApplicationContext().getBean(TeamDao.class);
     }
-    
+
     @Override
     protected void doPost(
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
         request.setCharacterEncoding("UTF-8");
-        
-        Member member = new Member();
-        member.setId(request.getParameter("id"));
-        member.setEmail(request.getParameter("email"));
-        member.setPassword(request.getParameter("password"));
+
+        Team team = new Team();
+        team.setName(request.getParameter("name"));
+        team.setDescription(request.getParameter("description"));
+        team.setMaxQty(Integer.parseInt(request.getParameter("maxQty")));
+        team.setStartDate(Date.valueOf(request.getParameter("startDate")));
+        team.setEndDate(Date.valueOf(request.getParameter("endDate")));
 
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -46,13 +48,13 @@ public class MemberAddServlet extends HttpServlet {
         out.println("<meta charset='UTF-8'>");
         out.println("<meta http-equiv='Refresh' content='1;url=list'>");
         
-        out.println("<title>회원 등록</title>");
+        out.println("<title>팀 등록</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>회원 등록 결과</h1>");
+        out.println("<h1>팀 등록 결과</h1>");
         
         try {
-            memberDao.insert(member);
+            teamDao.insert(team);
             out.println("<p>등록 성공!</p>");
         } catch (Exception e) {
             out.println("<p>등록 실패!</p>");
@@ -61,17 +63,16 @@ public class MemberAddServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
     }
-
 }
 
 //ver 37 - 컨트롤러를 서블릿으로 변경
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
-//ver 26 - MemberController에서 add() 메서드를 추출하여 클래스로 정의.
+//ver 26 - TeamController에서 add() 메서드를 추출하여 클래스로 정의.
 //ver 23 - @Component 애노테이션을 붙인다.
-//ver 22 - MemberDao 변경 사항에 맞춰 이 클래스를 변경한다.
-//ver 18 - ArrayList가 적용된 MemberDao를 사용한다.
-//         onMemberList()에서 배열의 각 항목에 대해 null 값을 검사하는 부분을 제거한다.
+//ver 22 - TaskDao 변경 사항에 맞춰 이 클래스를 변경한다.
+//ver 18 - ArrayList가 적용된 TeamDao를 사용한다.
 //ver 16 - 인스턴스 변수를 직접 사용하는 대신 겟터, 셋터 사용.
-// ver 15 - MemberDao를 생성자에서 주입 받도록 변경.
-// ver 14 - MemberDao를 사용하여 회원 데이터를 관리한다.
+// ver 15 - TeamDao를 생성자에서 주입 받도록 변경.
+// ver 14 - TeamDao를 사용하여 팀 데이터를 관리한다.
+// ver 13 - 시작일, 종료일을 문자열로 입력 받아 Date 객체로 변환하여 저장.
