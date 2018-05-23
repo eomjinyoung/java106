4f799b06637f5c1d8c0088460a934aa874ce5e46
eomjinyoung <jinyoung.eom@gmail.com>
2018-05-23 12:24:42 +0900

    .

--- java106-java-project/src/main/java/README.md
+++ java106-java-project/src/main/java/README.md
@@ -1,10 +1,6 @@
 # 변경 내역
-- 모든 서블릿 변경
-- CharacterEncodingFilter 추가 
-- ContextLoaderListener 추가
-- WebApplicationContextUtils 추가 
-- web.xml 변경 
-- InitServlet 제거
+- LoginServlet 추가
+
 
 
 
--- java106-java-project/src/main/java/bitcamp/java106/pms/servlet/auth/LoginServlet.java
+++ java106-java-project/src/main/java/bitcamp/java106/pms/servlet/auth/LoginServlet.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,18 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
+        
+        // 웹브라우저가 "id"라는 쿠키를 보냈으면 입력폼을 출력할 때 사용한다.
+        String id = "";
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("id")) {
+                    id = cookie.getValue();
+                    break;
+                }
+            }
+        }
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -32,21 +45,51 @@ public class LoginServlet extends HttpServlet {
         out.println("<form action='login' method='post'>");
         out.println("<table border='1'>");
         out.println("<tr><th>아이디</th>");
-        out.println("    <td><input type='text' name='id'></td></tr>");
+        out.printf("    <td><input type='text' name='id' value='%s'></td></tr>\n", id);
         out.println("<tr><th>암호</th>");
         out.println("    <td><input type='password' name='password'></td></tr>");
         out.println("</table>");
+        out.println("<p><input type='checkbox' name='saveId'> 아이디 저장</p>");
         out.println("<button>로그인</button>");
         out.println("</form>");
         out.println("</body>");
         out.println("</html>");
         
     }
+    
+    @Override
+    protected void doPost(
+            HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        
+        String id = request.getParameter("id");
+        String password = request.getParameter("password");
+        
+        Cookie cookie = null;
+        if (request.getParameter("saveId") != null) {
+            // 입력폼에서 로그인할 때 사용한 ID를 자동으로 출력할 수 있도록 
+            // 웹브라우저로 보내 저장시킨다.
+            cookie = new Cookie("id", id);
+            cookie.setMaxAge(60 * 60 * 24 * 7);
+        } else { // "아이디 저장" 체크박스를 체크하지 않았다면 
+            cookie = new Cookie("id", "");
+            cookie.setMaxAge(0); // 웹브라우저에 "id"라는 이름으로 저장된 쿠키가 있다면 제거한다.
+            // 즉 유효기간을 0으로 설정함으로써 "id"라는 이름의 쿠키를 무효화시키는 것이다.
+        }
+        response.addCookie(cookie);
+        
+        response.sendRedirect(request.getContextPath()); // => "/java106-java-project"
+    }
 }
 
-
-
-
+//               [웹브라우저]                                  [웹서버] 
+// GET 요청: /java106-java-project/auth/login ===>
+//                                                       <=== 응답: 로그인폼 
+// POST 요청: /java106-java-project/auth/login ===>
+//                                                       <=== 응답: redirect URL
+// GET 요청: /java106-java-project ===> 
+//                                                       <=== 응답: index.html
+// 메인화면 출력!
 
 
 
