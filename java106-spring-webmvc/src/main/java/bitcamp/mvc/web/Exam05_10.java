eaa79282f0b36183bfc2107bf6739e79c88be39e
eomjinyoung <jinyoung.eom@gmail.com>
2018-06-05 12:57:41 +0900

    .

--- java106-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam05_10.java
+++ java106-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam05_10.java
@@ -1,27 +1,34 @@
-// 요청 핸들러의 파라미터 - @RequestPart를 사용하여 multipart/form-data 형식으로 넘어온 값 꺼내기
+// 요청 핸들러의 파라미터 - multipart/form-data 형식으로 넘어온 값 꺼내기
 package bitcamp.mvc.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller 
 @RequestMapping("/exam05_10") 
 public class Exam05_10 {
     
-    // 멀티파트 형식으로 넘어 온 데이터를 꺼낼 때 
-    // 파라미터에 @RequestPart를 붙인다.
+    // 멀티파트 형식으로 넘어 온 데이터를 꺼낼 때는 기존의 파라미터 변수를 선언하는 방식과 같다.
+    // 주의!
+    // => Spring WebMVC에 멀티파트 데이터를 처리하는 객체를 등록해야 한다.
+    //    또한 그 객체가 의존하는 아파치의 commons-fileupload 라이브러리도 추가해야 한다.
     
     // 테스트: 
     // http://localhost:8888/java106-spring-webmvc/exam05_10.html
-    @PostMapping(value="m1")
+    @PostMapping(value="m1", produces="text/plain;charset=UTF-8")
     @ResponseBody
     public String m1(
-            String name, 
-            int age, 
-            String photo) {
-        return String.format("m1(): name=%s, age=%d, photo=%s", name, age, photo);
+            String name, // <input type="text" ...>
+            int age,  // <input type="number" ...>
+            MultipartFile photo // <input type="file" ...>
+            ) {
+        return String.format("m1(): name=%s, age=%d, photo=%s", 
+                name, 
+                age, 
+                photo.getOriginalFilename());
     }
     
    
