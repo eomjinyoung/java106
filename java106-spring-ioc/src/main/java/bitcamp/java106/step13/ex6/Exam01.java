0172f9a9d5ebdb09268e2f6e50ab2e254ffd7f79
eomjinyoung <jinyoung.eom@gmail.com>
2018-06-08 16:55:06 +0900

    .

--- java106-spring-ioc/src/main/java/bitcamp/java106/step13/ex6/Exam01.java
+++ java106-spring-ioc/src/main/java/bitcamp/java106/step13/ex6/Exam01.java
@@ -1,14 +1,14 @@
-// AOP(Aspect-Oriented Programming) - AOP 필터에서 타겟 객체의 메서드를 호출하기 전에 파라미터를 받거나 리턴 값을 받기
-package bitcamp.java106.step13.ex4;
+// AOP(Aspect-Oriented Programming) - 애노테이션으로 AOP 설정하기
+package bitcamp.java106.step13.ex6;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Exam01 {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext iocContainer = 
-                new ClassPathXmlApplicationContext(
-                        "bitcamp/java106/step13/ex4/application-context-01.xml");
+        AnnotationConfigApplicationContext iocContainer = 
+                new AnnotationConfigApplicationContext(AppConfig.class);
         
         String[] names = iocContainer.getBeanDefinitionNames();
         System.out.println("---------------------------------------");
