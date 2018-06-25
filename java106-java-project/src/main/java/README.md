51b11eb2cad6ef3dd286a0d083f9589dcbcef289
eomjinyoung <jinyoung.eom@gmail.com>
2018-06-25 12:05:55 +0900

    .

--- java106-java-project/src/main/java/README.md
+++ java106-java-project/src/main/java/README.md
@@ -1,6 +1,9 @@
 # 변경 내역
-- /WEB-INF/spring/dispatcher-servlet.xml 변경 
-- build.gradle 변경 
-  - aspectj-weaver 라이브러리 추가
+- /WEB-INF/spring/application-context.xml 추가
+- /WEB-INF/spring/json-servlet.xml 추가
+- /WEB-INF/spring/dispatcher-servlet.xml 변경
+- /WEB-INF/web.xml 변경 
+- bitcam.java106.pms.web.json 패키지 생성
+- ...json.BoardController 클래스 추가
  
 
---
+++ java106-java-project/src/main/java/bitcamp/java106/pms/web/json/BoardController.java
@@ -0,0 +1,78 @@
+package bitcamp.java106.pms.web.json;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.service.BoardService;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+    
+    BoardService boardService;
+    
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @RequestMapping("add")
+    public String add(Board board) throws Exception {
+        boardService.add(board);
+        return "redirect:list";
+    }
+    
+    @RequestMapping("delete")
+    public String delete(@RequestParam("no") int no) throws Exception {
+        
+        int count = boardService.delete(no);
+        if (count == 0) {
+            throw new Exception("해당 게시물이 없습니다.");
+        }
+        return "redirect:list";
+    }
+    
+    @RequestMapping("list{page}")
+    public Object list(
+            @MatrixVariable(defaultValue="1") int pageNo,
+            @MatrixVariable(defaultValue="3") int pageSize) {        
+        return boardService.list(pageNo, pageSize);
+    }
+    
+    @RequestMapping("update")
+    public String update(Board board) throws Exception {
+        
+        int count = boardService.update(board);
+        if (count == 0) {
+            throw new Exception("해당 게시물이 존재하지 않습니다.");
+        } 
+        return "redirect:list";
+    }
+    
+    @RequestMapping("{no}")
+    public String view(
+            @PathVariable int no, 
+            Map<String,Object> map) throws Exception {
+        
+        Board board = boardService.get(no);
+        if (board == null) {
+            throw new Exception("유효하지 않은 게시물 번호입니다.");
+        }
+        map.put("board", board);
+        return "board/view";
+    }
+
+}
+
+//ver 55 - JSON 데이터를 출력하는 페이지 컨트롤러 생성
+
+
+
+
+
+
