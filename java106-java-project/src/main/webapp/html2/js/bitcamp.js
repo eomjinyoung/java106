b235cafa11f549ed8089ed1782e74c1be35aba60
eomjinyoung <jinyoung.eom@gmail.com>
2018-06-26 10:18:32 +0900

    .

--- java106-java-project/src/main/webapp/html2/js/bitcamp.js
+++ java106-java-project/src/main/webapp/html2/js/bitcamp.js
@@ -1,7 +1,45 @@
 // 자바스크립트 함수를 모아 놓은 라이브러리
-var bitcamp = new Object();
+var jQuery = function(param) {
+	var tags;
+    if (param.startsWith("<")) {
+        var tagName = param.substr(1, param.length - 2); // <pre> ==> 0, (5-2)
+        tags = [document.createElement(tagName)];
+    } else if (param.startsWith(".")) {
+    	tags = document.querySelectorAll(param);
+    }
+    
+    tags.html = function(value) {
+    	for (var tag of tags) {
+    		tag.innerHTML = value;
+    	}
+    	return tags;
+    };
+    
+    tags.appendTo = function(parent) {
+    	for (var tag of tags) {
+    		parent.appendChild(tag);
+    	}
+    	return tags;
+    };
+    
+    tags.css = function(styleName, value) {
+    	for (var tag of tags) {
+    		tag.style[styleName] = value;
+    	}
+    	return tags;
+    };
+    
+    tags.val = function(value) {
+    	for (var tag of tags) {
+    		tag.style[styleName] = value;
+    	}
+    	return tags;
+    };
+    
+    return tags;
+};
 
-bitcamp.ajax = (url, settings) => {
+jQuery.ajax = (url, settings) => {
 	// AJAX 실행 정보를 초기화시킨다.
 	if (settings == undefined || settings == null) {
 		settings = {};
@@ -47,27 +85,27 @@ bitcamp.ajax = (url, settings) => {
 	}	
 };
 
-bitcamp.getJSON = (url, p1, p2) => {
+jQuery.getJSON = (url, p1, p2) => {
 	// 호출 예:
 	if (p1 == undefined || typeof p1 != "function") {
 		// => getJSON("board/list");
 		// => getJSON("board/list", {pageNo:1,pageSize:5});
 		// => getJSON("board/list", {pageNo:1,pageSize:5}, function(data) {...});
-	    bitcamp.ajax(url, {
+	    jQuery.ajax(url, {
 	    	"data": p1,
 	    	"dataType": "json",
 	    	"success": p2
 	    });
 	} else if (typeof p1 == "function") {
 		// => getJSON("board/list", function() {});
-		bitcamp.ajax(url, {
+		jQuery.ajax(url, {
 			"dataType": "json",
 	    	"success": p1
 	    });
 	}
 };
 
-var $ = bitcamp;
+var $ = jQuery;
 
 
 
