0125d9dbf1f577e5673d55d0a21f9896e02665ac
eomjinyoung <jinyoung.eom@gmail.com>
2018-07-19 15:47:15 +0900

    .

--- java106-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam11_2.java
+++ java106-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam11_2.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import net.coobird.thumbnailator.Thumbnails;
+
 @RestController 
 @RequestMapping("/exam11_2") 
 public class Exam11_2 {
@@ -97,6 +99,36 @@ public class Exam11_2 {
             }
         }
         return returnData;
+    }
+    
+    @PostMapping("upload04")
+    public Object upload04(
+            MultipartFile files) {
+        
+        HashMap<String,Object> jsonData = new HashMap<>();
+        
+        String filesDir = sc.getRealPath("/files");
+        
+        String filename = UUID.randomUUID().toString();
+        jsonData.put("filename", filename);
+        jsonData.put("filesize", files.getSize());
+        jsonData.put("originname", files.getOriginalFilename());
+        try {
+            File path = new File(filesDir + "/" + filename);
+            files.transferTo(path);
+            
+            // 써네일 이미지 생성
+            String thumbnailPath = path.getCanonicalPath() + "_50x50";
+            System.out.println(thumbnailPath);
+            Thumbnails.of(path)
+                      .size(50, 50)
+                      .outputFormat("jpg")
+                      .toFile(new File(thumbnailPath));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonData;
     }
 }
 
