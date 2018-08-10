68a388e640cc82f7832797940c20fd51ee20bfc2
eomjinyoung <jinyoung.eom@gmail.com>
2018-08-10 11:40:46 +0900

    .

--- java106-facebook-chatbot/app.js
+++ java106-facebook-chatbot/app.js
@@ -76,7 +76,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
