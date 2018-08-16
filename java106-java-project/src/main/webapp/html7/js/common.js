79ff2f1d898fcf85598bb81a4ae4aab8aaac081a
eomjinyoung <jinyoung.eom@gmail.com>
2018-08-16 15:58:06 +0900

    '.'

--- java106-java-project/src/main/webapp/html7/js/common.js
+++ java106-java-project/src/main/webapp/html7/js/common.js
@@ -1,1 +1,2 @@
-var serverRoot = "http://localhost:8888/java106-java-project";
+//var serverRoot = "http://localhost:8888/java106-java-project";
+var serverRoot = "http://13.209.35.254:8888/java106-java-project";
